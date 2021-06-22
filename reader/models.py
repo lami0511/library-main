4c717d48f010512c2f3430399cf521054e1dbda7
lami <may9305111@gmail.com>
2021-06-23 02:21:59 +0800

    3

--- reader/models.py
+++ reader/models.py
@@ -8,7 +8,7 @@ class Reader(Model):
     tel = CharField('聯絡電話', max_length=255)
     email = EmailField('電子信箱')
     titleclass = CharField('職稱/任教科目', max_length=255)
-    status = CharField('教室借用', max_length=255)
+    status = CharField('教室借用(請手動填寫早上/下午/晚上，第幾節課~第幾節課使用)', max_length=255)
 
     def __str__(self):
         return "{} / {} / {}".format(
